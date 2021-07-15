f152599a5068570393ed0462da466df21276982c
MakRyul <pig052656@naver.com>
2021-07-15 14:32:50 +0900

    Upload

---
+++ N과M_2(15650번)/N과M.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int output[9];
+bool arr[9] = { false, };
+
+void NandM(int index) {
+	if (index > M) {
+		for (int i = 1; i <= M; i++) {
+			cout << output[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 1; i <= N; i++) {
+			if (arr[i] == false && output[index-1] < i) {
+				arr[i] = true;
+				output[index] = i;
+				NandM(index + 1);
+				arr[i] = false;
+			}
+		}
+	}
+}
+
+void input() {
+	cin >> N >> M;
+}
+
+void solve() {
+	NandM(1);
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solve();
+
+	return 0;
+}
