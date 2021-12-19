233df3ff264b9e84a887f863e1a1a463dbd17a76
MakRyul <pig052656@naver.com>
2021-12-19 17:08:57 +0900

    Upload

---
+++ 2xn타일링(11727번)/타일링.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int dp[1001];
+
+int solve(int n) {
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+	}
+
+	return dp[n];
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int answer;
+
+	cin >> N;
+	dp[1] = 1;
+	dp[2] = 3;
+
+	answer = solve(N);
+
+	cout << answer;
+
+	return 0;
+}
